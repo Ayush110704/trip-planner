b55a2bea69b92addc3c62a3e82ec3f8717b46b4a
Ayush sengar <aayussengar.work@gmail.com>
2025-12-19 16:44:31 +0530

    trip

--- src/AddTrip.jsx
+++ src/AddTrip.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Bounce, Slide, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; 
-6
+
 export default function AddTrip({ onTripAdded }) {
   const [form, setForm] = useState({
     tripName: "", boarding: "", destination: "", startDate: "", endDate: "",
